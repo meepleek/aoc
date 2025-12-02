fe138e37d6e7afa8538a166498eaa62790a2d6f2
meepleek <ergocrab@gmail.com>
2025-12-02 22:20:33 +0100

    feat: part_a

--- 2025/rust/day-1/src/lib.rs
+++ 2025/rust/day-1/src/lib.rs
@@ -1,7 +1,25 @@
 pub mod solution {
     #[tracing::instrument(skip(input))]
     pub fn part_a(input: &str) -> anyhow::Result<String> {
-        todo!("a")
+        let nums: Vec<isize> = input
+            .lines()
+            .map(|l| {
+                let (dir, num) = l.split_at(1);
+                let sign = if dir == "L" { -1 } else { 1 };
+                let num: isize = num.parse().expect("Valid num");
+                num * sign
+            })
+            .collect();
+        let mut dial = 50;
+        let mut res = 0;
+        for num in nums {
+            dial = (dial + num).rem_euclid(100);
+            if dial == 0 {
+                res += 1;
+            }
+        }
+
+        Ok(res.to_string())
     }
 
     #[tracing::instrument(skip(input))]
@@ -16,8 +34,8 @@ mod tests {
     use tracing_test::traced_test;
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
-    const EXPECTED_A: &str = "todo_expected_a";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_A: &str = "3";
+    const EXPECTED_B: &str = "6";
 
     #[test]
     #[traced_test]
