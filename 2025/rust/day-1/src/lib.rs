6b197c537c5b81a5cf9cbf2e041daf97eebd21b1
meepleek <ergocrab@gmail.com>
2025-12-03 14:58:43 +0100

    feat: part b

--- 2025/rust/day-1/src/lib.rs
+++ 2025/rust/day-1/src/lib.rs
@@ -24,7 +24,42 @@ pub mod solution {
 
     #[tracing::instrument(skip(input))]
     pub fn part_b(input: &str) -> anyhow::Result<String> {
-        todo!("b")
+        let nums: Vec<isize> = input
+            .lines()
+            .map(|l| {
+                let (dir, num) = l.split_at(1);
+                let sign = if dir == "L" { -1 } else { 1 };
+                let num: isize = num.parse().expect("Valid num");
+                num * sign
+            })
+            .collect();
+
+        let mut dial = 50;
+        let mut res = 0;
+        for mut num in nums {
+            res += num.abs() / 100;
+            num = num.wrapping_rem(100);
+            if num == 0 {
+                continue;
+            }
+            let prev = dial;
+            dial += num;
+            if dial == 0 {
+                res += 1;
+            } else if dial < 0 {
+                dial = 100 - dial.abs();
+                if prev != 0 {
+                    res += 1;
+                }
+            } else if dial >= 100 {
+                dial -= 100;
+                if prev != 0 {
+                    res += 1;
+                }
+            }
+        }
+
+        Ok(res.to_string())
     }
 }
 
