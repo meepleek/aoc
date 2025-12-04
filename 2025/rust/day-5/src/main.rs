39884688527618dc3d457421531ab953848115a7
meepleek <ergocrab@gmail.com>
2025-12-04 23:02:36 +0100

    chore: scaffold day 5

---
+++ 2025/rust/day-5/src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+
+use clap::Parser;
+
+use aoc_client::get_input;
+use day_5::solution::{part_a, part_b};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Run part b
+    #[arg(short)]
+    b: bool,
+}
+
+#[tokio::main]
+#[tracing::instrument]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let cli = Cli::parse();
+    let input = get_input(env::current_dir()?, 5).await?;
+    let output = if cli.b {
+        part_b(&input)?
+    } else {
+        part_a(&input)?
+    };
+    println!("{output}");
+    Ok(())
+}
