1d612fd156eae003bfbf672cd2a8eef6bc2ff06e
meepleek <ergocrab@gmail.com>
2025-12-04 23:02:36 +0100

    chore: scaffold day 5

---
+++ 2025/rust/day-5/src/lib.rs
@@ -0,0 +1,35 @@
+pub mod solution {
+    #[tracing::instrument(skip(input))]
+    pub fn part_a(input: &str) -> anyhow::Result<String> {
+        todo!("a")
+    }
+
+    #[tracing::instrument(skip(input))]
+    pub fn part_b(input: &str) -> anyhow::Result<String> {
+        todo!("b")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tracing_test::traced_test;
+
+    const TEST_INPUT: &str = include_str!("../inputs/example.txt");
+    const EXPECTED_A: &str = "todo_expected_a";
+    const EXPECTED_B: &str = "todo_expected_b";
+
+    #[test]
+    #[traced_test]
+    fn day_5_a() {
+        let res = solution::part_a(TEST_INPUT);
+        assert_eq!(EXPECTED_A, res.unwrap());
+    }
+
+    #[test]
+    #[traced_test]
+    fn day_5_b() {
+        let res = solution::part_b(TEST_INPUT);
+        assert_eq!(EXPECTED_B, res.unwrap());
+    }
+}
