bf1a999204636cf2c13a58bfd3cb344d6459ae17
meepleek <ergocrab@gmail.com>
2025-12-04 22:35:38 +0100

    feat: part a

--- 2025/rust/day-4/src/lib.rs
+++ 2025/rust/day-4/src/lib.rs
@@ -1,7 +1,19 @@
 pub mod solution {
+    pub use grid::prelude::*;
+
     #[tracing::instrument(skip(input))]
     pub fn part_a(input: &str) -> anyhow::Result<String> {
-        todo!("a")
+        let grid = GridBuilder::<()>::build_obstacle_grid()
+            .input(input)
+            .obstacle('.')
+            .call()?
+            .grid;
+        let count = grid
+            .walkable_tiles()
+            .keys()
+            .filter(|t| grid.neighbours_8(**t).len() < 4)
+            .count();
+        Ok(count.to_string())
     }
 
     #[tracing::instrument(skip(input))]
@@ -16,7 +28,7 @@ mod tests {
     use tracing_test::traced_test;
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
-    const EXPECTED_A: &str = "todo_expected_a";
+    const EXPECTED_A: &str = "13";
     const EXPECTED_B: &str = "todo_expected_b";
 
     #[test]
