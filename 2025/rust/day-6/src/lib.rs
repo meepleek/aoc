1aa90f8f959ea886031f5d618c3845117ac8ead1
meepleek <ergocrab@gmail.com>
2025-12-06 21:47:25 +0100

    feat: part a

--- 2025/rust/day-6/src/lib.rs
+++ 2025/rust/day-6/src/lib.rs
@@ -1,7 +1,45 @@
 pub mod solution {
+    use anyhow::Context;
+
+    #[derive(Debug)]
+    enum ColumnOp {
+        Add,
+        Mul,
+    }
+
     #[tracing::instrument(skip(input))]
     pub fn part_a(input: &str) -> anyhow::Result<String> {
-        todo!("a")
+        let ops: Vec<_> = input
+            .lines()
+            .rev()
+            .next()
+            .context("Ops line")?
+            .split_whitespace()
+            .map(|op| match op {
+                "+" => ColumnOp::Add,
+                "*" => ColumnOp::Mul,
+                _ => panic!("Unknown op {op}"),
+            })
+            .collect();
+        let mut col_results: Vec<_> = ops
+            .iter()
+            .map(|op| match op {
+                ColumnOp::Add => 0,
+                ColumnOp::Mul => 1,
+            })
+            .collect();
+        for l in input.lines().rev().skip(1) {
+            for (i, num) in l.split_whitespace().enumerate() {
+                let num: usize = num.parse()?;
+                let current = col_results[i];
+                col_results[i] = match ops[i] {
+                    ColumnOp::Add => current + num,
+                    ColumnOp::Mul => current * num,
+                };
+            }
+        }
+        let total: usize = col_results.into_iter().sum();
+        Ok(total.to_string())
     }
 
     #[tracing::instrument(skip(input))]
@@ -16,7 +54,7 @@ mod tests {
     use tracing_test::traced_test;
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
-    const EXPECTED_A: &str = "todo_expected_a";
+    const EXPECTED_A: &str = "4277556";
     const EXPECTED_B: &str = "todo_expected_b";
 
     #[test]
