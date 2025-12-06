09f20d7669555aa59726cc2bf712c3b5cc22ca32
meepleek <ergocrab@gmail.com>
2025-12-06 13:44:46 +0100

    refactor: day 5 - use range fns from helper crates

--- 2025/rust/day-5/src/lib.rs
+++ 2025/rust/day-5/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod solution {
-    use std::ops::RangeInclusive;
+    use parse::range::parse_inclusive_range;
+    use range::merge_inclusive_ranges;
 
     #[tracing::instrument(skip(input))]
     pub fn part_a(input: &str) -> anyhow::Result<String> {
-        let mut parsing_ranges = true;
+        let var_name = true;
+        let mut parsing_ranges = var_name;
         let mut ranges = Vec::with_capacity(1000);
         // let mut ids = Vec::with_capacity(1000);
         let mut res = 0;
@@ -14,10 +16,7 @@ pub mod solution {
             }
 
             if parsing_ranges {
-                let (from, to) = l.split_once('-').expect("valid range");
-                ranges.push(
-                    from.parse::<usize>().expect("valid from")..=to.parse().expect("valid to"),
-                );
+                ranges.push(parse_inclusive_range(l)?);
             } else {
                 let num = l.parse::<usize>().expect("is num");
                 // ids.push();
@@ -39,8 +38,7 @@ pub mod solution {
                     return None;
                 }
 
-                let (from, to) = l.split_once('-').expect("valid range");
-                Some(from.parse::<usize>().expect("valid from")..=to.parse().expect("valid to"))
+                Some(parse_inclusive_range::<usize>(l).expect("valid range"))
             })
             .collect();
         ranges.sort_by_key(|r| *r.start());
@@ -55,7 +53,7 @@ pub mod solution {
                     if i == j {
                         continue;
                     }
-                    if let Some(merged_range) = merge_ranges(&ranges[i], &ranges[j]) {
+                    if let Some(merged_range) = merge_inclusive_ranges(&ranges[i], &ranges[j]) {
                         // remove from end
                         ranges.remove(i.max(j));
                         ranges.remove(j.min(i));
@@ -71,29 +69,15 @@ pub mod solution {
         let total: usize = ranges.into_iter().map(|r| r.end() - r.start() + 1).sum();
         Ok(total.to_string())
     }
-
-    pub(super) fn merge_ranges(
-        a: &RangeInclusive<usize>,
-        b: &RangeInclusive<usize>,
-    ) -> Option<RangeInclusive<usize>> {
-        match true {
-            _ if a.start() <= b.start() && a.end() >= b.end() => Some(a.clone()),
-            _ if b.start() <= a.start() && b.end() >= a.end() => Some(b.clone()),
-            _ if a.end() >= b.start() && a.end() <= b.end() => Some(*a.start()..=*b.end()),
-            _ if b.end() >= a.start() && b.end() <= a.end() => Some(*b.start()..=*a.end()),
-            _ => None,
-        }
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rstest::rstest;
-    use std::ops::RangeInclusive;
     use tracing_test::traced_test;
 
-    const TEST_INPUT: &str = include_str!("../inputs/example.txt");
+    const VAR_NAME: &str = include_str!("../inputs/example.txt");
+    const TEST_INPUT: &str = VAR_NAME;
     const EXPECTED_A: &str = "3";
     const EXPECTED_B: &str = "14";
 
@@ -109,24 +93,5 @@ mod tests {
     fn day_5_b() {
         let res = solution::part_b(TEST_INPUT);
         assert_eq!(EXPECTED_B, res.unwrap());
-    }
-
-    #[rstest]
-    #[case(0..=1, 2..=3, None)]
-    #[case(0..=1, 1..=3, Some(0..=3))]
-    #[case(0..=5, 1..=10, Some(0..=10))]
-    #[case(0..=1, 0..=3, Some(0..=3))]
-    #[case(0..=100, 10..=11, Some(0..=100))]
-    #[traced_test]
-    fn merge_ranges(
-        #[case] a: RangeInclusive<usize>,
-        #[case] b: RangeInclusive<usize>,
-        #[case] expected_result: Option<RangeInclusive<usize>>,
-    ) {
-        let merged = solution::merge_ranges(&a, &b);
-        let merged_reverse = solution::merge_ranges(&b, &a);
-
-        assert_eq!(expected_result, merged);
-        assert_eq!(expected_result, merged_reverse);
     }
 }
