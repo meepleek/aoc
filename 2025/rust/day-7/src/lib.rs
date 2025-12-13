aa1d3e3abbbde593f805670a9b4c4d82d20edf75
meepleek <ergocrab@gmail.com>
2025-12-13 12:15:33 +0100

    feat: part a

--- 2025/rust/day-7/src/lib.rs
+++ 2025/rust/day-7/src/lib.rs
@@ -1,7 +1,39 @@
 pub mod solution {
+    use std::collections::HashSet;
+
+    use anyhow::Context;
+
     #[tracing::instrument(skip(input))]
     pub fn part_a(input: &str) -> anyhow::Result<String> {
-        todo!("a")
+        let lines: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+        let start_line = &lines[0];
+        let width = start_line.len();
+        let start_x = start_line
+            .iter()
+            .position(|c| *c == 'S')
+            .context("start tile")?;
+        let mut beams = HashSet::with_capacity(width);
+        beams.insert(start_x);
+        let mut line_beams = Vec::with_capacity(width);
+        let mut split_count = 0;
+        for l in lines.iter().skip(1) {
+            for x in &beams {
+                if l[*x] == '^' {
+                    split_count += 1;
+                    if *x > 0 {
+                        line_beams.push(x - 1);
+                    }
+                    if *x < (width - 1) {
+                        line_beams.push(x + 1);
+                    }
+                } else {
+                    line_beams.push(*x);
+                }
+            }
+            beams.clear();
+            beams.extend(line_beams.drain(..));
+        }
+        Ok(split_count.to_string())
     }
 
     #[tracing::instrument(skip(input))]
@@ -16,8 +48,8 @@ mod tests {
     use tracing_test::traced_test;
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
-    const EXPECTED_A: &str = "todo_expected_a";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_A: &str = "21";
+    const EXPECTED_B: &str = "40";
 
     #[test]
     #[traced_test]
