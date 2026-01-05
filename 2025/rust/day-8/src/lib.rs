2b9c3dbaabc226a0a5013f3958a9df7946c6c055
meepleek <ergocrab@gmail.com>
2026-01-05 23:41:56 +0100

    feat: part b WIP

--- 2025/rust/day-8/src/lib.rs
+++ 2025/rust/day-8/src/lib.rs
@@ -112,7 +112,86 @@ pub mod solution {
 
     #[tracing::instrument(skip(input))]
     pub fn part_b(input: &str) -> anyhow::Result<String> {
-        todo!("b")
+        let coords: Vec<_> = input
+            .lines()
+            .map(|l| {
+                let (x, rest) = l.split_once(',').unwrap();
+                let (y, z) = rest.split_once(',').unwrap();
+                [
+                    x.parse::<f32>().unwrap(),
+                    y.parse().unwrap(),
+                    z.parse().unwrap(),
+                ]
+            })
+            .collect();
+        tracing::info!(coords_len = coords.len());
+
+        let mut kdtree = KdTree::new(3);
+        for (i, c) in coords.iter().enumerate() {
+            kdtree.add(c, i).unwrap();
+        }
+        let mut distances = BinaryHeap::new();
+
+        for (a_i, a) in coords.iter().enumerate() {
+            let nearest = kdtree
+                .iter_nearest(a, &squared_euclidean)
+                .unwrap()
+                .skip(1) // skip connection to self (0 dist)
+                .map(|(dist_sq, b_i)| DistIndex {
+                    dist_sq,
+                    index_a: a_i,
+                    index_b: *b_i,
+                });
+            distances.extend(nearest);
+        }
+
+        let mut circuits: Vec<HashSet<usize>> = Vec::with_capacity(1000);
+        loop {
+            let nearest = distances.pop().expect("Nearest is available");
+            _ = distances.pop(); // nearest pairs come in... um, pairs, so discard the dupe
+            match (
+                circuits
+                    .iter()
+                    .position(|circ| circ.contains(&nearest.index_a)),
+                circuits
+                    .iter()
+                    .position(|circ| circ.contains(&nearest.index_b)),
+            ) {
+                (Some(circ_a), Some(circ_b)) => {
+                    if circ_a != circ_b {
+                        let min = circ_a.min(circ_b);
+                        let max = circ_a.max(circ_b);
+                        let to_merge = circuits.remove(max);
+                        circuits[min].extend(to_merge);
+                    }
+                }
+                (Some(circ_a), None) => {
+                    circuits[circ_a].insert(nearest.index_a);
+                    circuits[circ_a].insert(nearest.index_b);
+                }
+                (None, Some(circ_b)) => {
+                    circuits[circ_b].insert(nearest.index_a);
+                    circuits[circ_b].insert(nearest.index_b);
+                }
+                (None, None) => {
+                    circuits.push(HashSet::from([nearest.index_a, nearest.index_b]));
+                }
+            }
+
+            if circuits.len() == 1 && circuits[0].len() == coords.len() {
+                let a_x = coords[nearest.index_a][0];
+                let b_x = coords[nearest.index_b][0];
+                tracing::warn!(a_x, b_x);
+                // tracing::warn!(?circuits);
+                tracing::warn!(circuit_lens=?circuits.iter().map(|c| c.len()).collect::<Vec<_>>());
+                return Ok((a_x * b_x).to_string());
+            } else {
+                tracing::warn!(circuit_count = circuits.len());
+                if circuits.len() == 2 {
+                    tracing::warn!(circuit_lens=?circuits.iter().map(|c| c.len()).collect::<Vec<_>>());
+                }
+            }
+        }
     }
 }
 
@@ -123,7 +202,7 @@ mod tests {
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
     const EXPECTED_A: &str = "40";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_B: &str = "25272";
 
     #[test]
     #[traced_test]
