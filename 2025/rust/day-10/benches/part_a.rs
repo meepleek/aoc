4f9d6877d678ee5f2328b7480273f6b4cd153245
meepleek <ergocrab@gmail.com>
2026-01-06 23:07:53 +0100

    chore: scaffold day 10

---
+++ 2025/rust/day-10/benches/part_a.rs
@@ -0,0 +1,17 @@
+use aoc_client::block_on_input;
+use criterion::{criterion_group, criterion_main, Criterion};
+use day_10::solution::part_a;
+use std::hint::black_box;
+
+fn part_a_benchmark(c: &mut Criterion) {
+    let input = block_on_input(10);
+    let mut group = c.benchmark_group("day-10");
+    // group
+    //     .sample_size(10)
+    //     .measurement_time(std::time::Duration::from_secs(30));
+    group.bench_function("part_a", |b| b.iter(|| part_a(black_box(&input))));
+    group.finish();
+}
+
+criterion_group!(benches, part_a_benchmark);
+criterion_main!(benches);
