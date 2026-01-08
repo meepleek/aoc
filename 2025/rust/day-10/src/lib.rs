a04f9de2b96902a7c11f8fe546a9a88cf014ab60
meepleek <ergocrab@gmail.com>
2026-01-08 08:44:35 +0100

    feat: part b - naive approach WIP
    Way too slow for the actual input. Just the first machine/row took
    almost 6 hrs to process and there're trickier lines in the input for
    sure...

--- 2025/rust/day-10/src/lib.rs
+++ 2025/rust/day-10/src/lib.rs
@@ -13,28 +13,64 @@ pub mod solution {
 
     #[derive(Debug, PartialEq, Eq)]
     pub(super) struct Machine {
-        pub lights: u16,
-        pub buttons: Vec<u16>,
+        pub light_mask: u16,
+        pub buttons: Vec<Vec<u16>>,
+        pub button_masks: Vec<u16>,
         pub joltage: Vec<u16>,
     }
     impl Machine {
-        pub(super) fn min_presses(&self) -> usize {
+        pub(super) fn min_presses_lights(&self) -> usize {
             for combo_size in 1..=32 {
+                if self
+                    .button_masks
+                    .iter()
+                    .combinations_with_replacement(combo_size)
+                    .any(|buttons| {
+                        buttons
+                            .iter()
+                            .fold::<u16, _>(0, |acc, btn_mask| acc ^ **btn_mask)
+                            == self.light_mask
+                    })
+                {
+                    return combo_size;
+                }
+            }
+
+            panic!("Could not find combo for machine {self:?}");
+        }
+
+        // todo: this is way too slow for the actual input
+        // try: switch to DFS with neibour nodes/btns ordered based on rarest joltages among them to narrow down the possibility space 
+        pub(super) fn min_presses_joltage(&self) -> usize {
+            let max_joltage = *self.joltage.iter().max().unwrap() as usize;
+            for combo_size in max_joltage..(max_joltage * 10) {
                 if self
                     .buttons
                     .iter()
                     .combinations_with_replacement(combo_size)
                     .any(|buttons| {
-                        buttons.iter().fold::<u16, _>(0, |acc, btn_bitmap| {
-                            // tracing::warn!(
-                            //     ?buttons,
-                            //     lights = self.lights,
-                            //     acc,
-                            //     btn_bitmap,
-                            //     new = acc ^ **btn_bitmap
-                            // );
-                            acc ^ **btn_bitmap
-                        }) == self.lights
+                        let mut joltage = self.joltage.clone();
+                        for btn in buttons {
+                            let mut check = false;
+                            for i in btn {
+                                let i = *i as usize;
+                                if joltage[i] == 0 {
+                                    // would go over
+                                    return false;
+                                }
+
+                                let val = joltage[i] - 1;
+                                joltage[i] = val;
+                                if val == 0 {
+                                    check = true;
+                                }
+                            }
+                            if check && joltage.iter().all(|j| *j == 0) {
+                                return true;
+                            }
+                        }
+
+                        false
                     })
                 {
                     return combo_size;
@@ -58,7 +94,11 @@ pub mod solution {
                 Self::parse_joltage,
             ))(input)?;
             let machine = Self {
-                lights,
+                light_mask: lights,
+                button_masks: buttons
+                    .iter()
+                    .map(|set_bits| set_bits.into_iter().fold(0, |acc, bit_i| acc | 1 << bit_i))
+                    .collect(),
                 buttons,
                 joltage,
             };
@@ -81,14 +121,12 @@ pub mod solution {
             Ok((rest, light_pattern))
         }
 
-        pub(super) fn parse_btn(input: &str) -> IResult<&str, u16> {
-            let (rest, set_bits) = delimited(
+        pub(super) fn parse_btn(input: &str) -> IResult<&str, Vec<u16>> {
+            delimited(
                 char('('),
                 separated_list1(char(','), character::complete::u16),
                 char(')'),
-            )(input)?;
-            let mask = set_bits.into_iter().fold(0, |acc, bit_i| acc | 1 << bit_i);
-            Ok((rest, mask))
+            )(input)
         }
 
         fn parse_joltage(input: &str) -> IResult<&str, Vec<u16>> {
@@ -105,14 +143,19 @@ pub mod solution {
         let machines = Machine::parse_list(input)?;
         let res = machines
             .into_iter()
-            .map(|machine| machine.min_presses())
+            .map(|machine| machine.min_presses_lights())
             .sum::<usize>();
         Ok(res.to_string())
     }
 
     #[tracing::instrument(skip(input))]
     pub fn part_b(input: &str) -> anyhow::Result<String> {
-        todo!("b")
+        let machines = Machine::parse_list(input)?;
+        let res = machines
+            .into_iter()
+            .map(|machine| machine.min_presses_joltage())
+            .sum::<usize>();
+        Ok(res.to_string())
     }
 }
 
@@ -126,7 +169,7 @@ mod tests {
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
     const EXPECTED_A: &str = "7";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_B: &str = "33";
 
     #[test]
     #[traced_test]
@@ -150,11 +193,11 @@ mod tests {
         assert_eq!(pattern, 0b0110);
     }
 
-    #[test_case("(3)" => 0b1000)]
-    #[test_case("(0,1)" => 0b0011)]
-    #[test_case("(1,3)" => 0b1010)]
+    #[test_case("(3)" => vec![3])]
+    #[test_case("(0,1)" => vec![0, 1])]
+    #[test_case("(1,3)" => vec![1, 3])]
     #[traced_test]
-    fn parse_btn(input: &str) -> u16 {
+    fn parse_btn(input: &str) -> Vec<u16> {
         Machine::parse_btn(input).unwrap().1
     }
 
@@ -167,8 +210,16 @@ mod tests {
             (
                 "\n",
                 Machine {
-                    lights: 0b0110,
-                    buttons: vec![0b1000, 0b1010, 0b0100, 0b1100, 0b0101, 0b0011],
+                    light_mask: 0b0110,
+                    button_masks: vec![0b1000, 0b1010, 0b0100, 0b1100, 0b0101, 0b0011],
+                    buttons: vec![
+                        vec![3],
+                        vec![1, 3],
+                        vec![2],
+                        vec![2, 3],
+                        vec![0, 2],
+                        vec![0, 1],
+                    ],
                     joltage: vec![3, 5, 4, 7]
                 }
             ),
@@ -184,13 +235,15 @@ mod tests {
         assert_eq!(
             vec![
                 Machine {
-                    lights: 0b1000,
-                    buttons: vec![0b0001],
+                    light_mask: 0b1000,
+                    button_masks: vec![0b0001],
+                    buttons: vec![vec![0]],
                     joltage: vec![1]
                 },
                 Machine {
-                    lights: 0b0100,
-                    buttons: vec![0b0010],
+                    light_mask: 0b0100,
+                    button_masks: vec![0b0010],
+                    buttons: vec![vec![1]],
                     joltage: vec![2]
                 }
             ],
@@ -200,32 +253,71 @@ mod tests {
 
     #[test_case(
         Machine {
-            lights: 6,
-            buttons: vec![8, 10, 4, 12, 5, 3],
-            joltage: vec![3, 5, 4, 7]
-        } => 2; "row 1")]
+            light_mask: 6,
+            button_masks: vec![8, 10, 4, 12, 5, 3],
+            buttons: vec![],
+            joltage: vec![]
+        } => 2; "lights - row 1")]
     #[test_case(
         Machine {
-            lights: 8,
-            buttons: vec![29, 12, 17, 7, 30],
-            joltage: vec![7, 5, 12, 7, 2]
-        } => 3; "row 2")]
+            light_mask: 8,
+            button_masks: vec![29, 12, 17, 7, 30],
+            buttons: vec![],
+            joltage: vec![]
+        } => 3; "lights - row 2")]
     #[test_case(
         Machine {
-            lights: 46,
-            buttons: vec![31, 25, 55, 6],
-            joltage: vec![10, 11, 11, 5, 10, 5]
-        } => 2; "row 3")]
+            light_mask: 46,
+            button_masks: vec![31, 25, 55, 6],
+            buttons: vec![],
+            joltage: vec![]
+        } => 2; "lights - row 3")]
     #[traced_test]
-    fn min_presses(machine: Machine) -> usize {
-        machine.min_presses()
+    fn min_presses_lights(machine: Machine) -> usize {
+        machine.min_presses_lights()
     }
-    //
 
-    // [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-    // todo tests:
-    // parser
-    // bitmask conversion
-    // applying a bitmask/button
-    // resolve single line/machine
+    #[test_case(
+        Machine {
+            light_mask: 6,
+            button_masks: vec![8, 10, 4, 12, 5, 3],
+            buttons: vec![
+                vec![3],
+                vec![1, 3],
+                vec![2],
+                vec![2, 3],
+                vec![0, 2],
+                vec![0, 1],
+            ],
+            joltage: vec![3, 5, 4, 7]
+        } => 10; "joltage - row 1")]
+    #[test_case(
+        Machine {
+            light_mask: 8,
+            button_masks: vec![29, 12, 17, 7, 30],
+            buttons: vec![
+                vec![0, 2, 3, 4],
+                vec![2, 3],
+                vec![0, 4],
+                vec![0, 1, 2],                
+                vec![1, 2, 3, 4],
+            ],
+            joltage: vec![7, 5, 12, 7, 2]
+        } => 12; "joltage - row 2")]
+    #[test_case(
+        Machine {
+            light_mask: 46,
+            button_masks: vec![31, 25, 55, 6],
+            buttons: vec![
+                vec![0, 1, 2, 3, 4],
+                vec![0, 3, 4],
+                vec![0, 1, 2, 4, 5], 
+                vec![1, 2],                
+            ],
+            joltage: vec![10, 11, 11, 5, 10, 5]
+        } => 11; "joltage - row 3")]
+    #[traced_test]
+    fn min_presses_joltage(machine: Machine) -> usize {
+        machine.min_presses_joltage()
+    }
 }
